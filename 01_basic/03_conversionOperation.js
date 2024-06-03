5ef8b76008d939e403d5b590fc87d01c448612d7
vjyrawat <156895848+vjyrawat@users.noreply.github.com>
2024-06-03 04:25:44 +0000

    Conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+let isLoggedIn = ""
+let valueInBoolean = Boolean(isLoggedIn)
+
+console.log(valueInBoolean);
+
+// 1 => true; 0 => false
+// "" => false
+// "Vijay" => true
+
